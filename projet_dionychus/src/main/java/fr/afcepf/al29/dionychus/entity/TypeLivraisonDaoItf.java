9a17c90520b957d0d41f65f5a5bd30b68986e07d
Popchewb <gernigon.nicolas@gmail.com>
2016-12-14 10:04:51 +0100

    Delete TypeLivraisonDaoItf.java

--- projet_dionychus/src/main/java/fr/afcepf/al29/dionychus/entity/TypeLivraisonDaoItf.java
+++
@@ -1,9 +0,0 @@
-package fr.afcepf.al29.dionychus.data.itf;
-
-import java.util.List;
-
-import fr.afcepf.al29.dionychus.entity.TypeLivraison;
-
-public interface TypeLivraisonDaoItf {
-	List<TypeLivraison> getAll();
-}
