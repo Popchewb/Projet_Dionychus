98cb05db969685cddfe2b01b3414e888a28afe95
Popchewb <gernigon.nicolas@gmail.com>
2016-12-14 10:05:24 +0100

    Delete TypeVinDaoItf.java

--- projet_dionychus/src/main/java/fr/afcepf/al29/dionychus/entity/TypeVinDaoItf.java
+++
@@ -1,9 +0,0 @@
-package fr.afcepf.al29.dionychus.data.itf;
-
-import java.util.List;
-
-import fr.afcepf.al29.dionychus.entity.TypeVin;
-
-public interface TypeVinDaoItf {
-	List<TypeVin> getAll();
-}
