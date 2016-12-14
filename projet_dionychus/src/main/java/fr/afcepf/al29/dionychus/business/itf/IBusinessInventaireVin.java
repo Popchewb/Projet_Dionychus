2b247c3652da053bb78d8cbd73f970ca9497f5dd
Popchewb <gernigon.nicolas@gmail.com>
2016-12-14 10:21:22 +0100

    Add files via upload

---
+++ projet_dionychus/src/main/java/fr/afcepf/al29/dionychus/business/itf/IBusinessInventaireVin.java
@@ -0,0 +1,11 @@
+package fr.afcepf.al29.dionychus.business.itf;
+
+import java.util.List;
+
+import fr.afcepf.al29.dionychus.entity.Vin;
+
+public interface IBusinessInventaireVin {
+
+	List<Vin> getAllVins();
+
+}
