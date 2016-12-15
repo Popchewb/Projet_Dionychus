bf1d8d174c63d5578b38a51cd89710584be72536
Popchewb <gernigon.nicolas@gmail.com>
2016-12-15 14:55:43 +0100

    maj Nico 14h55

--- projet_dionychus/src/main/java/fr/afcepf/al29/dionychus/controller/VinManagedBean.java
+++ projet_dionychus/src/main/java/fr/afcepf/al29/dionychus/controller/VinManagedBean.java
@@ -1,6 +1,8 @@
 package fr.afcepf.al29.dionychus.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,38 +14,119 @@ import fr.afcepf.al29.dionychus.entity.Arome;
 import fr.afcepf.al29.dionychus.entity.Cepage;
 import fr.afcepf.al29.dionychus.entity.Region;
 import fr.afcepf.al29.dionychus.entity.TypeVin;
+import fr.afcepf.al29.dionychus.entity.Vin;
 
 @Controller("vinManagedBean")
 @Scope("request")
 public class VinManagedBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Autowired
 	private IBusinessInventaire proxyInventaire;
-	
+
+	private String vigneron;
+
+	private String millesime;
+
 	private List<TypeVin> typeVins;
 
 	private Integer typeVin;
-	
+
 	private Double prix;
-	
+
 	private List<Arome> aromes;
-	
+
 	private String arome;
-	
+
 	private List<Cepage> cepages;
-	
+
 	private String cepage;
-	
+
 	private List<Region> regions;
-	
+
 	private Integer region;
-	
+
 	public String rechercher() {
+		List<Vin> vins = proxyInventaire.getAllVin();
+		List<Vin> selectVin = new ArrayList<>();
+
+		if(typeVin != 0) {
+			for (Vin vin : vins) {
+				if(vin.getTypeVin().getIdTypeVin() != typeVin) {
+					selectVin.add(vin);
+				}
+			}
+			vins.removeAll(selectVin);
+			selectVin.clear();
+		}
+
+		if(!millesime.equals("")){
+			for (Vin vin : vins) {
+				if(vin.getAnnee() != Integer.parseInt(millesime)){
+					selectVin.add(vin);
+				}
+			}
+			vins.removeAll(selectVin);
+			selectVin.clear();
+		}
+
+//		if(prix != 0) {
+//			for (Vin vin : vins) {
+//				if(vin.getPrix() < prix) {
+//					System.out.println("boucle if prix " + vin.getPrix() + " " + prix);
+//					selectVin.add(vin);
+//				}
+//			}
+//			vins.removeAll(selectVin);
+//			selectVin.clear();
+//		}
+
+		if(region != 0) {
+			for (Vin vin : vins) {
+				if(vin.getRegion().getIdRegion() != region) {
+					selectVin.add(vin);
+				}
+			}
+			vins.removeAll(selectVin);
+			selectVin.clear();
+		}
+		
+		System.out.println("debut methode");
+		System.out.println("arome " + arome);
+
+		if(!arome.equals("")){
+			boolean estPresent = false;
+			String[] chaineAromes = arome.split(",");
+			for (Vin vin : vins) {
+				System.out.println("vin " + vin);
+				for (String str : chaineAromes) {
+					System.out.println("str " + str);
+					for (Arome aro : vin.getAromes()) {
+						System.out.println("arome " + aro.getLibelle());
+						if(aro.getLibelle().equals(str)){
+							System.out.println("est Present");
+							estPresent = true;
+						}
+					}
+					if(!estPresent){
+						System.out.println("ajout vin");
+						selectVin.add(vin);
+					}
+				}
+			}
+			System.out.println("taille " + vins.size() + " " + selectVin.size());
+			vins.removeAll(selectVin);
+			selectVin.clear();
+		}
+		
+		for (Vin vin : vins) {
+			System.out.println(vin);	
+		}
+
 		return null;
 	}
-	
+
 	public List<TypeVin> getTypeVins() {
 		return proxyInventaire.getAllTypeVin();
 	}
@@ -116,6 +199,20 @@ public class VinManagedBean implements Serializable {
 		this.region = region;
 	}
 
-	
+	public String getVigneron() {
+		return vigneron;
+	}
+
+	public void setVigneron(String vigneron) {
+		this.vigneron = vigneron;
+	}
+
+	public String getMillesime() {
+		return millesime;
+	}
+
+	public void setMillesime(String millesime) {
+		this.millesime = millesime;
+	}
 
 }
