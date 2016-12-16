ac9bb3e9fc10ea4d2f7ee1567de481fa08c16ccd
AntoineHZ <antoine.huet@gmx.com>
2016-12-16 14:33:36 +0100

    fat maj ahz 3

--- projet_dionychus/src/main/java/fr/afcepf/al29/dionychus/data/itf/UtilisateurDaoItf.java
+++ projet_dionychus/src/main/java/fr/afcepf/al29/dionychus/data/itf/UtilisateurDaoItf.java
@@ -5,19 +5,9 @@ import java.util.List;
 import fr.afcepf.al29.dionychus.entity.Utilisateur;
 
 public interface UtilisateurDaoItf {
-
 	List<Utilisateur> getAll();
-
 	Utilisateur getById(int paramIdUtilisateur);
-<<<<<<< HEAD
-
-	void addUtilisateur(Utilisateur paramUtilisateur);
-
-=======
 	Utilisateur addUtilisateur(Utilisateur paramUtilisateur);
->>>>>>> 142e74daf1681f155f6256c7f5669b5ea8c70852
 	void updateUtilisateur(Utilisateur paramUtilisateur);
-
 	void deleteUtilisateur(Integer paramIdUtilisateur);
-
 }
